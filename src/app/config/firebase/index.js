df4db0fc59025d6241a8850e7825b2faac909713
Shaun Saker <shaun@aux.co.za>
2019-07-30 15:07:16 +0200

    Add Firebase config

--- src/app/config/firebase/index.js
+++ src/app/config/firebase/index.js
@@ -4,6 +4,7 @@ const databaseURL = process.env.REACT_APP_DATABASE_URL;
 const projectId = process.env.REACT_APP_PROJECT_ID;
 const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
 const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
 
 const firebase = {
   apiKey,
@@ -12,6 +13,7 @@ const firebase = {
   projectId,
   storageBucket,
   messagingSenderId,
+  appId,
 };
 
 export default firebase;
