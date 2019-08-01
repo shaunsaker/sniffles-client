fde841e34c931bab4bafde5207038cb0bd6ed01f
Shaun Saker <shaun@aux.co.za>
2019-08-01 11:07:57 +0200

    Add HeaderBar logic

--- src/app/components/HeaderBar/index.js
+++ src/app/components/HeaderBar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Router from 'next/router';
 
 import HeaderBar from './HeaderBar';
 
@@ -8,20 +9,51 @@ export class HeaderBarContainer extends React.Component {
   constructor(props) {
     super(props);
 
+    this.onSignOut = this.onSignOut.bind(this);
+    this.signOut = this.signOut.bind(this);
+
     this.state = {};
   }
 
-  static propTypes = {};
+  static propTypes = {
+    /*
+     * Connect
+     */
+    dispatch: PropTypes.func,
+    authenticated: PropTypes.bool,
+  };
 
   static defaultProps = {};
 
+  onSignOut() {
+    this.signOut();
+    Router.replace('/login');
+  }
+
+  signOut() {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'signOut',
+    });
+  }
+
   render() {
-    return <HeaderBar />;
+    const { authenticated } = this.props;
+    const handleSignOut = authenticated ? this.onSignOut : null;
+
+    return <HeaderBar handleSignOut={handleSignOut} />;
   }
 }
 
 function mapStateToProps(state) {
-  return {};
+  const { user } = state;
+  const { email } = user;
+  const authenticated = email ? true : false;
+
+  return {
+    authenticated,
+  };
 }
 
 export default connect(mapStateToProps)(HeaderBarContainer);
